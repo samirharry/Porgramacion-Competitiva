928590f6a3f137c7edd2905ad02fb0fdb253f8e2
Samir Harry Huamán Patricio <shuamanp@uni.pe>
2020-01-05 19:17:25 -0500

    Nueva seccion añadida

--- Geeks for geeks/Searching Algorithms/jumpSearch.cpp
+++ Geeks for geeks/Searching Algorithms/jumpSearch.cpp
@@ -1,15 +1,43 @@
 #include <bits/stdc++.h>
-#define debug(x) cout<<#x<<" => "<<x<<endl;
-#define debugArray(x,n) for(ll i=0;i<n;i++)  cout<<#x<<"["<<i<<" => "<<x[i]<<endl;
+#define debug(x) cout << #x << " => " << x << endl;
+#define debugArray(x, n)       \
+	for (ll i = 0; i < n; i++) \
+		cout << #x << "[" << i << " => " << x[i] << endl;
 
 using namespace std;
 
 typedef long long ll;
 
-ll jumpSearch()
+ll jumpSearch(ll arr[], ll x, ll n)
+{
+	ll step = sqrt(n);
+	ll prev = 0;
 
+	while (arr[min(step, n) - 1] < x)
+	{
+		prev = step;
+		step += sqrt(n);
+		if (prev >= n)
+			return -1;
+	}
 
-int main(){
+	while (arr[prev]< x)
+	{
+		prev++;
+		if(prev == min(step,n)) return -1;
+	}
 
+	if(arr[prev] == x){
+		return prev;
+	}
+	return x;
+}
+
+int main()
+{
+	ll n = 6;
+	ll arr[n] = {1, 1, 2, 3, 5, 9};
+	ll x = 1;
+	cout << jumpSearch(arr, x, n) << endl;
 	return 0;
 }
