df2e5d367c325959a5e086ce59bb87b8a1fdb90c
samirharry <shuamanp@uni.pe>
2020-08-16 15:35:12 -0500

    Codeforces

---
+++ CodeChef/CHEFDILDilemma.cpp
@@ -0,0 +1,27 @@
+#include <bits/stdc++.h>
+
+#define debug(x) cout<<#x<<"=>"<<x<<endl;
+#define debugArray(x,n) for(int i = 0; i<n;i++) cout<<#x<<"["<<i<<"] => "<<x[i]<<endl;
+typedef long long ll;
+typedef long double ld;
+
+using namespace std;
+
+// COidgo mandado por samirharry
+
+int main(){
+	ios_base::sync_with_stdio(false);
+	cin.tie(NULL);
+	cout.tie(NULL);
+	ll q;
+	cin>>q;
+	while(q--){
+		string aux;
+		cin>>aux;
+		ll cantUno = 0;
+		for(ll i=0;i<aux.length();i++) if(aux[i] == '1') cantUno++;
+		if(cantUno&1 ==1) cout<<"WIN"<<endl;
+		else cout<<"LOSE"<<endl;
+	}
+	return 0;
+}
---
+++ CodeChef/KSGuddu.cpp
@@ -0,0 +1,26 @@
+#include <bits/stdc++.h>
+
+#define debug(x) cout<<#x<<"=>"<<x<<endl;
+#define debugArray(x,n) for(int i = 0; i<n;i++) cout<<#x<<"["<<i<<"] => "<<x[i]<<endl;
+typedef long long ll;
+typedef long double ld;
+
+using namespace std;
+
+int main(){
+	ios_base::sync_with_stdio(false);
+	cin.tie(NULL);
+	cout.tie(NULL);
+	ll t;
+	cin>>t;
+	while(t--){
+		ll n;
+		cin>>n;
+		ll arr[n];
+		for(ll i=0;i<n;i++){
+			cin>>arr[i];
+		}
+		
+	}
+	return 0;
+}
